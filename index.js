cd0dfe41d56c60a00f95ba1e47a3ac6b1c731dcc
Govind <you@example.com>
2024-06-15 11:35:09 +0530

    updating_code

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 const express=require("express")
 const dotenv = require("dotenv");
+const mongoose = require('mongoose');
 dotenv.config();
 const app=express();
 
 
 
 const PORT=process.env.PORT;
+const MONGO_URL=process.env.MONGO_URL
 app.get('/',async(req,res)=>{
     return res.status(200).send({message:"ohk"})
 })
 app.get('/check',(req,res)=>{
     return res.status(200).send({message:"working everything"})
+})
+
+mongoose.connect(MONGO_URL)
+.then(()=>{
+    console.log('DB connected');
+})
+.catch((err)=>{
+    console.log(`Error connecting to DB: ${err}`);
 })
 
 app.listen(PORT,(req,res)=>{
