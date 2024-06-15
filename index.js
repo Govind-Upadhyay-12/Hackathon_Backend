eaab387fd58a287b678f85b92f989c0e20f00db8
Govind <you@example.com>
2024-06-15 11:24:32 +0530

    update

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ const PORT=process.env.PORT;
 app.get('/',async(req,res)=>{
     return res.status(200).send({message:"ohk"})
 })
+app.get('/check',(req,res)=>{
+    return res.status(200).send({message:"working everything"})
+})
 
 app.listen(PORT,(req,res)=>{
     console.log(`server is running on port ${PORT}`)
